8697d9c8b260ece3e65221c0861747393cbbf799
y-nakata <trident0310@gmail.com>
2019-12-02 14:10:53 +0900

    Fogポストエフェクト

---
+++ Assets/PostEffect.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostEffect : MonoBehaviour {
+	public Material mosaic;
+	void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        Graphics.Blit(src, dest, mosaic);
+    }
+}
